b50853b03a8160d340e62bce2e90b4d4b3c4473d
Alex Mekhovov <alex.mehovov@gmail.com>
2012-07-12 15:08:48 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendsLife::Application.config.secret_token = 'fa08d7cdced07b20b1043ed11cb5e2e452925609a1723c71a803deae2a2676d3c495e30c59746ce44f339330b41aa292e35bf851d8d33541532852c6743be634'
