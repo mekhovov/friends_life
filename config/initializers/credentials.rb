94954e8990f60f1da673a8af7d03c1e823f6fa6e
Alex Mekhovov <alex.mehovov@gmail.com>
2012-07-16 20:39:36 +0300

    fix: creds and style

--- config/initializers/credentials.rb
+++ config/initializers/credentials.rb
@@ -1,5 +1,12 @@
 
 #### Twitter ####
+if Rails.env.production? 
+  ENV['TWITTER_KEY']    =  'KXwPiG50xkU2Ht7xK3lMw'
+  ENV['TWITTER_SECRET'] = '2MrLUlPg6Hl9A9zbozYDe3NUPsuu8lGkyg9UDq08U'
+end
 
-ENV['TWITTER_KEY']    =  'KXwPiG50xkU2Ht7xK3lMw'
-ENV['TWITTER_SECRET'] = '2MrLUlPg6Hl9A9zbozYDe3NUPsuu8lGkyg9UDq08U'
+if Rails.env.development? || Rails.env.test?
+  ENV['TWITTER_KEY']    =  'Ys70uFYPuT70umBopJmryA'
+  ENV['TWITTER_SECRET'] =  '6AoC95IewUiesSKl4QXfOFGRHSUrLNDWfnNewqN8'
+end
+
