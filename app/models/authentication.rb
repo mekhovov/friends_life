a199df6cbd462590dec6d7a9e8f621341d55eb93
Alex Mekhovov <alex.mehovov@gmail.com>
2012-07-17 21:54:06 +0300

    modify: design, header, menu

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,4 +1,8 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
   attr_accessible :provider, :uid, :user_id
+
+  def self.providers
+    %w[twitter facebook foursquare github linkedin]
+  end
 end
