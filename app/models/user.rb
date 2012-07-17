cf7614882322628044af455d8a37756cf348c3fa
Alex Mekhovov <alex.mehovov@gmail.com>
2012-07-17 15:22:45 +0300

    fix: form style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   
-  has_many :authentications
+  has_many :authentications, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
